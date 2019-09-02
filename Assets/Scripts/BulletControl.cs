a31f801e37996705b8718ca4d1348b190a2b0d04
elisasaltori <elisa.trujillo@usp.br>
2019-09-02 18:22:43 -0300

    bubble shoot + bubble destroyed

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class BulletControl : MonoBehaviour
+{
+
+    [Header("Attributes")]
+    public float range = 10f;
+    public bool isPlayerBullet;
+    public string enemyTag = null;
+    public string playerTag = null;
+
+    //used for checking if range has been reached
+    private Vector3 spawnLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //destroys itself if range is reached
+        ShotDistance();
+    }
+
+    void ShotDistance()
+    {
+        if (Vector3.Distance(spawnLocation, transform.position) > range)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //hit an enemy, destroy enemy and begin bubbling up
+        if(enemyTag!=null && collision.gameObject.CompareTag(enemyTag))
+        {
+            Debug.Log("hit an enemy!");
+        }
+        else
+        {
+            //hit a player, call stun
+            if (playerTag != null && collision.gameObject.CompareTag(playerTag))
+            {
+                Debug.Log("hit a player!");
+            }
+                
+        }
+
+        //destroy bullet on collision 
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GravityGun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGun : MonoBehaviour
+{
+
+    [Header("Attributes")]
+    public float speed = 10.0f; //shooting speed
+    public float spawnDist = 0.5f;
+
+    [System.Serializable]
+    public struct Controls
+    {
+        public KeyCode shoot;
+    }
+
+    [Header("Controls")]
+    public Controls controls;
+
+    [Header("References")]
+    public GameObject gravityFieldPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown(controls.shoot))
+        {
+            Shoot();
+            Debug.Log("Shoot");
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(gravityFieldPrefab);
+        bullet.transform.position = this.transform.position + transform.right * spawnDist;
+        bullet.GetComponent<Rigidbody2D>().velocity = speed * transform.right;
+    }
+}
