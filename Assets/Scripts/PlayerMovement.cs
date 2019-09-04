bd0f6d6ee5bc19c381e7252d7d95b24367e2e6cf
pedroavellar <pedro.avellar@hotmail.com>
2019-09-03 21:07:20 -0300

    Moving robot

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Este codigo controla o movimento do jogador
+public class PlayerMovement : MonoBehaviour
+{
+    // Atributos
+    [Header("Attributes")]
+    public float speed = 20;
+    public float jumpForce = 500;
+    public bool facingRight = true;
+    public LayerMask isGround;
+
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator animator;
+
+    private float horizontalMove;
+    private float verticalMove;
+
+    // Chamado antes de start
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!facingRight)
+        {
+            flipSprite();
+            facingRight = !facingRight;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+    }
+
+    // Used for physics, fixed intervals
+    private void FixedUpdate()
+    {
+
+        rb.AddForce(Vector2.right * horizontalMove * speed);
+        //Vector2 vect = rb.velocity;
+        //rb.velocity = new Vector2(horizontalMove * speed, vect.y);
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+        if (horizontalMove > 0 && !facingRight)
+        {
+            flipSprite();
+        }
+        else if (horizontalMove < 0 && facingRight)
+        {
+            flipSprite();
+        }
+
+        // Se nao estiver no ar e existir movimento vertical, pula
+        if (verticalMove > 0 && isOnGround())
+        {
+            animator.SetTrigger("jumping");
+            rb.AddForce(Vector2.up * verticalMove * jumpForce);
+        }
+    }
+
+    // Inverte no eixo-x o sprite
+    void flipSprite()
+    {
+        facingRight = !facingRight;
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    //Checa se esta "pisando"
+    public bool isOnGround()
+    {
+        //bool groundCheck = Physics2D.Raycast(new Vector2(transform.position.x,
+        // transform.position.y - height),
+        // -Vector2.up, rayCastLength);
+        //if (groundCheck) return true;
+        //if (rb.velocity.y == 0) return true;
+        //return false;
+
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 1.25f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, isGround);
+        if (hit.collider != null && rb.velocity.y == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    [Header("Attributes")]
+    public float speed = 10.0f; //shooting speed
+    public float spawnDist = 0.05f;
+    public float dir = 1;
+
+    private Animator animator;
+
+    [System.Serializable]
+    public struct Controls
+    {
+        public KeyCode shoot;
+    }
+
+    [Header("Controls")]
+    public Controls controls;
+
+    [Header("References")]
+    public GameObject bomb;
+
+    private bool facing;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+        //dir = gameObject.GetComponent<PlayerMovement>().facingRight;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(controls.shoot))
+        {
+            animator.SetTrigger("shooting");
+            Shoots();
+        }
+    }
+
+    private void Shoots()
+    {
+        facing = gameObject.GetComponent<PlayerMovement>().facingRight;
+        if (facing)
+        {
+            dir = 1;
+        } else
+        {
+            dir = -1;
+        }
+
+        GameObject bullet = Instantiate(bomb);
+        bullet.transform.position = (this.transform.position + transform.right * (dir *spawnDist));
+        bullet.GetComponent<Rigidbody2D>().velocity = dir * (speed * transform.right);
+    }
+}
