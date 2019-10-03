294e69b47b3367e5a7af2c884ed9eea49f494c10
elisasaltori <elisa.trujillo@usp.br>
2019-10-03 09:23:35 -0300

    countdown working

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -1,23 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
+    [Header("Attributes")]
     public float timeStart = 180f;
+    public float runningOutOfTime = 10f;
+    public string endSceneName;
+
+    [Header("References")]
     public Text textBox;
+
+
+
+    private AudioSource timerSound;
+    private float time;
+    private bool timeEnding;
+
+    private void Awake()
+    {
+        timerSound = GetComponent<AudioSource>();
+    }
 
     // Use this for initialization
     void Start()
     {
         textBox.text = timeStart.ToString();
+        time = timeStart;
+        timeEnding = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeStart -= Time.deltaTime;
-        textBox.text = Mathf.Round(timeStart).ToString();
+        time -= Time.deltaTime;
+        textBox.text = Mathf.Round(time).ToString();
+
+        if(time <= runningOutOfTime && !timeEnding)
+        {
+            timeEnding = true;
+            textBox.color = Color.red;
+            timerSound.Play();
+        }
+
+
+        //match time is over
+        if(time <= 0)
+        {
+            SceneManager.LoadScene(endSceneName);
+        }
     }
 }
