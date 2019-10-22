5447ef33a52de14f317071113a14863b921b3715
Bruno Homma <bmhomma@gmail.com>
2019-10-22 19:58:00 -0300

    Implementação do tiro dos mosntros

---
+++ Assets/EnemyShootTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootTime : MonoBehaviour
+{
+
+	private EnemyWeapon weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon = GetComponent<EnemyWeapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (weapon != null) {
+        	weapon.Attack();
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemyShoot.cs
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -1,48 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/**
- * The player's gravity gun. Shoots "gravity fields" on input
- **/
 public class EnemyShoot : MonoBehaviour
 {
-
     [Header("Attributes")]
-    public float speed = 10.0f; //shooting speed
-    public float spawnDist = 0.05f;
+    public int damage = 1;
+    public string objectTag;
 
-    [System.Serializable]
-    public struct Controls
-    {
-        public KeyCode shoot;
-    }
-
-    [Header("Controls")]
-    public Controls controls;
-
-    [Header("References")]
-    public GameObject gravityFieldPrefab;
-
-    // Update is called once per frame
     void Update()
     {
-        GetInput();
+
     }
 
-    void GetInput()
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (Input.GetKeyDown(controls.shoot))
+        if (collider.gameObject.tag == objectTag) 
         {
-            Shoot();
-            Debug.Log("Shoot");
+            Destroy(gameObject);
         }
-    }
-
-    void Shoot()
-    {
-        GameObject bullet = Instantiate(gravityFieldPrefab);
-        bullet.transform.position = this.transform.position + transform.right * spawnDist;
-        bullet.GetComponent<Rigidbody2D>().velocity = speed * transform.right;
     }
 }
