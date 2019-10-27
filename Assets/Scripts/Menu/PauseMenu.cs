760dad4cac9d043f9b2d6d7ac2a2d87df21a95d8
GabrielaRossales <cisco.rossales@gmail.com>
2019-10-27 20:44:30 -0300

    ingame pause menu(press escape key to appear)

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))//if Escape key is pressed
+        {
+            if (gameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;//resumes the game at normal speed
+        gameIsPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;//stops the game
+        gameIsPaused = true;
+    }
+}
